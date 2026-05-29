be08de762c92dbc2d11830304a8a13b80a896814
tjdgns0618 <gruddufs@gmail.com>
2026-05-29 13:31:10 +0900

    Initial commit

---
+++ src/main/java/com/example/team3trimcommercepaymentproject/Team3trimCommercePaymentProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.team3trimcommercepaymentproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team3trimCommercePaymentProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team3trimCommercePaymentProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/team3trimcommercepaymentproject/Team3trimCommercePaymentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team3trimcommercepaymentproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team3trimCommercePaymentProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
