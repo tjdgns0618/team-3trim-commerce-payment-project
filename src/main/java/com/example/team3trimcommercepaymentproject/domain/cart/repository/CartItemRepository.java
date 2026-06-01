3054af7edf717c0393e001d19a673c557a32fc5b
tjdgns0618 <gruddufs@gmail.com>
2026-06-01 17:43:13 +0900

    feat(Cart): 회원 ID와 장바구니 상품 ID로 장바구니 상품을 찾는 쿼리 메서드 추가

--- src/main/java/com/example/team3trimcommercepaymentproject/domain/cart/repository/CartItemRepository.java
+++ src/main/java/com/example/team3trimcommercepaymentproject/domain/cart/repository/CartItemRepository.java
@@ -11,5 +11,5 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 	// 회원 고유 번호와 상품 고유 번호로 기존 장바구니 상품 조회 (중복 상품 담기 판별용)
 	Optional<CartItem> findByMemberIdAndProductId(Long memberId, Long productId);
 
-
+	Optional<CartItem> findByMemberIdAndId(Long memberId, Long id);
 }
