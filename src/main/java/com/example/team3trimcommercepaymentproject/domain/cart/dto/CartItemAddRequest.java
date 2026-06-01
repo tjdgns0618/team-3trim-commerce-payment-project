8aff7955dd7babfbaee05cfcbcf2d00cea8f8d51
tjdgns0618 <gruddufs@gmail.com>
2026-06-01 15:26:24 +0900

    feat(cart): 장바구니 DTO 추가

---
+++ src/main/java/com/example/team3trimcommercepaymentproject/domain/cart/dto/CartItemAddRequest.java
@@ -0,0 +1,4 @@
+package com.example.team3trimcommercepaymentproject.domain.cart.dto;
+
+public record CartItemAddRequest(Long productId, Integer quantity) {
+}
---
+++ src/main/java/com/example/team3trimcommercepaymentproject/domain/cart/dto/CartItemAddResponse.java
@@ -0,0 +1,4 @@
+package com.example.team3trimcommercepaymentproject.domain.cart.dto;
+
+public record CartItemAddResponse(Long cartItemId) {
+}
