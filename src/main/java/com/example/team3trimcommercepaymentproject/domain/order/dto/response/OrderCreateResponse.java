38fca93de5ba73ee22552e14be97f1aefad8e98c
hwang67120-bit <hwang67120@gmail.com>
2026-06-02 17:09:08 +0900

    feat: Oder 도메인 롤백으로 인한 수정

--- src/main/java/com/example/team3trimcommercepaymentproject/domain/order/dto/response/OrderCreateResponse.java
+++ src/main/java/com/example/team3trimcommercepaymentproject/domain/order/dto/response/OrderCreateResponse.java
@@ -1,6 +1,7 @@
 package com.example.team3trimcommercepaymentproject.domain.order.dto.response;
 
 import com.example.team3trimcommercepaymentproject.domain.order.entity.OrderStatus;
+import com.example.team3trimcommercepaymentproject.domain.payment.dto.response.PaymentCreateResponse;
 
 public record OrderCreateResponse(
         Long orderId,
