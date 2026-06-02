289969fe4620d5d8c46872116147ff1f3736f068
hjh <hjh690@naver.com>
2026-06-02 14:21:19 +0900

    feat : PointTransaction 생성

---
+++ src/main/java/com/example/team3trimcommercepaymentproject/domain/pointTransaction/service/PointTransactionService.java
@@ -0,0 +1,32 @@
+package com.example.team3trimcommercepaymentproject.domain.pointTransaction.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.team3trimcommercepaymentproject.domain.member.entity.Member;
+import com.example.team3trimcommercepaymentproject.domain.member.repository.MemberRepository;
+import com.example.team3trimcommercepaymentproject.domain.member.service.MemberService;
+import com.example.team3trimcommercepaymentproject.domain.payment.entity.Payment;
+import com.example.team3trimcommercepaymentproject.domain.pointTransaction.entity.PointTransaction;
+import com.example.team3trimcommercepaymentproject.domain.pointTransaction.repository.PointTransactionRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class PointTransactionService {
+
+	private final MemberService memberService;
+	private final PointTransactionRepository pointTransactionRepository;
+
+	public void earnPoint(Long memberId, Payment payment, Long amount) {
+		Member member = memberService.addPoint(memberId, amount);
+		pointTransactionRepository.save(PointTransaction.earn(member, payment, amount));
+	}
+
+	public void usePoint(Long memberId, Payment payment, Long amount) {
+		Member member = memberService.usePoint(memberId, amount);
+		pointTransactionRepository.save(PointTransaction.use(member, payment, amount));
+	}
+}
