f76546134c5c778aa12dd9b5846d3f2fb17b40c8
hwang67120-bit <hwang67120@gmail.com>
2026-06-05 01:59:17 +0900

    feat: PortOneClient bulb: 해결방안 확인 후 수정

--- src/main/java/com/example/team3trimcommercepaymentproject/domain/payment/portOne/PortOneClient.java
+++ src/main/java/com/example/team3trimcommercepaymentproject/domain/payment/portOne/PortOneClient.java
@@ -2,6 +2,7 @@ package com.example.team3trimcommercepaymentproject.domain.payment.portOne;
 
 import com.example.team3trimcommercepaymentproject.domain.payment.dto.response.PortOnePaymentResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -11,9 +12,16 @@ public class PortOneClient {
 
     private final WebClient portOneWebClient;
 
+    @Value("${portone.store-id}")
+    private String storeId;
+
     public PortOnePaymentInfo getPayment(String portonePaymentId) {
         return portOneWebClient.get()
-                .uri("/payments/{paymentId}", portonePaymentId)
+                .uri(uriBuilder -> uriBuilder
+                        .path("/payments/{paymentId}")
+                        .queryParam("storeId", storeId)
+                        .build(portonePaymentId)
+                )
                 .retrieve()
                 .bodyToMono(PortOnePaymentInfo.class)
                 .block();
