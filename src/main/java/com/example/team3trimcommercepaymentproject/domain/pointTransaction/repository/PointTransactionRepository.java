99d8a602c7d414ae771262e28f6937765ebebda8
hjh <hjh690@naver.com>
2026-06-04 12:18:54 +0900

    feat : 포인트 내역 전체 조회

--- src/main/java/com/example/team3trimcommercepaymentproject/domain/pointTransaction/repository/PointTransactionRepository.java
+++ src/main/java/com/example/team3trimcommercepaymentproject/domain/pointTransaction/repository/PointTransactionRepository.java
@@ -1,5 +1,7 @@
 package com.example.team3trimcommercepaymentproject.domain.pointTransaction.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.example.team3trimcommercepaymentproject.domain.pointTransaction.entit
 
 @Repository
 public interface PointTransactionRepository extends JpaRepository<PointTransaction, Long> {
+	List<PointTransaction> findAllByMemberIdOrderByModifiedAtDesc(Long memberId);
 }
