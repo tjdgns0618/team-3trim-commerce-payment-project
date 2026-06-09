b312a93ebbd94145262ce76aa95072eb3833ff9f
hwang67120-bit <hwang67120@gmail.com>
2026-06-09 14:22:55 +0900

    feat: createOrderWithPayment 부분 수

--- src/main/java/com/example/team3trimcommercepaymentproject/domain/payment/controller/PaymentController.java
+++ src/main/java/com/example/team3trimcommercepaymentproject/domain/payment/controller/PaymentController.java
@@ -19,15 +19,15 @@ public class PaymentController {
     private final PaymentFacade paymentFacade;
     private final PortOneWebhookHandler portOneWebhookHandler;
 
-    @PostMapping("/confirm")
-    public ResponseEntity<ApiResponse<PaymentConfirmResponse>> confirm(
-            @AuthenticationPrincipal Long memberId,
-            @RequestBody PaymentConfirmRequest request
-    ) {
-
-        PaymentConfirmResponse response = paymentFacade.confirm(memberId, request);
-        return ResponseEntity.ok(ApiResponse.ok(response));
-    }
+//    @PostMapping("/confirm")
+//    public ResponseEntity<ApiResponse<PaymentConfirmResponse>> confirm(
+//            @AuthenticationPrincipal Long memberId,
+//            @RequestBody PaymentConfirmRequest request
+//    ) {
+//
+//        PaymentConfirmResponse response = paymentFacade.confirm(memberId, request);
+//        return ResponseEntity.ok(ApiResponse.ok(response));
+//    }
 
     @PostMapping("/webhooks/portone")
     public ResponseEntity<ApiResponse<Void>> receiveWebhook(
@@ -37,5 +37,20 @@ public class PaymentController {
         portOneWebhookHandler.handle(signature, body);
 
         return ResponseEntity.ok(ApiResponse.ok(null));
+    }
+
+    @PostMapping("/confirm")
+    public ResponseEntity<?> confirm(
+            @AuthenticationPrincipal Long memberId,
+            @RequestBody PaymentConfirmRequest request
+    ) {
+        try {
+            PaymentConfirmResponse response = paymentFacade.confirm(memberId, request);
+            return ResponseEntity.ok(ApiResponse.ok(response));
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(500)
+                    .body(e.getClass().getSimpleName() + ": " + e.getMessage());
+        }
     }
 }
